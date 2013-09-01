09a3f76d16199087e8a56682281272728f7962cd
Alex Cappiello <acappiel@andrew.cmu.edu>
2013-09-01 18:21:40 -0400

    Very basic io working.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

--- Project1/src/main/java/processmigration/io/TransactionalFileInputStream.java
+++ Project1/src/main/java/processmigration/io/TransactionalFileInputStream.java
@@ -1,26 +1,64 @@
 package processmigration.io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+/*
+ * First Attempt at TransactionalFileInputStream:
+ * 
+ * TODO:
+ *   - Serialization.
+ * 
+ * Ways to extend things:
+ *   - Keep the file open until migrating. Need some way to indicate this.
+ *   - Buffered IO. Need to flush before migrating.
+ */
 
 public class TransactionalFileInputStream extends InputStream implements
 		Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5132783333806695091L;
+	private File file;
+	private long position;
 
-	public TransactionalFileInputStream(String s) {
-		// TODO Auto-generated constructor stub
+	public TransactionalFileInputStream(String name)
+			throws FileNotFoundException {
+		File f = new File(name);
+		if (f.isDirectory()) {
+			throw new FileNotFoundException("File is a directory: " + name);
+		} else if (!f.isFile()) {
+			throw new FileNotFoundException("File does not exist: " + name);
+		} else if (!f.canRead()) {
+			throw new FileNotFoundException("Cannot read from file: " + name);
+		}
+		file = f;
+		position = 0;
 	}
 
 	@Override
 	public int read() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+		int data;
+		FileInputStream f = new FileInputStream(file);
+		try {
+			f.skip(position);
+			data = f.read();
+			if (data != -1) { // EOF.
+				position++;
+			}
+		} catch (IOException e) {
+			throw new IOException("Failed to read from file " + file.getPath()
+					+ " : " + e.getMessage());
+		} finally {
+			if (f != null) {
+				f.close();
+			}
+		}
+		return data;
 	}
 
 	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
--- Project1/src/main/java/processmigration/io/TransactionalFileOutputStream.java
+++ Project1/src/main/java/processmigration/io/TransactionalFileOutputStream.java
@@ -1,26 +1,70 @@
 package processmigration.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectStreamException;
 import java.io.OutputStream;
 import java.io.Serializable;
 
+/*
+ * First Attempt at TransactionalFileOutputStream:
+ * 
+ * TODO:
+ *   - Serialization.
+ * 
+ * Ways to extend things:
+ *   - Keep the file open until migrating. Need some way to indicate this.
+ *   - Buffered IO. Need to flush before migrating.
+ */
+
 public class TransactionalFileOutputStream extends OutputStream implements
 		Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8310483022289606755L;
+	private File file;
 
-	public TransactionalFileOutputStream(String s, boolean b) {
-		// TODO Auto-generated constructor stub
+	public TransactionalFileOutputStream(String name)
+			throws FileNotFoundException, IOException {
+		this(name, false);
+	}
+
+	public TransactionalFileOutputStream(String name, boolean append)
+			throws FileNotFoundException, IOException {
+		File f = new File(name);
+		if (f.isDirectory()) {
+			throw new FileNotFoundException("File is a directory: " + name);
+		}
+		if (!f.exists()) {
+			f.createNewFile();
+		}
+		if (!f.canWrite()) {
+			throw new FileNotFoundException("Cannot write to file: " + name);
+		}
+		// Is there a more sensible way to accomplish this?
+		if (!append) {
+			FileOutputStream out = new FileOutputStream(f);
+			out.write(0);
+			out.close();
+		}
+		file = f;
 	}
 
 	@Override
-	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-
+	public void write(int b) throws IOException {
+		// Always append to the file.
+		FileOutputStream f = new FileOutputStream(file, true);
+		try {
+			f.write(b);
+		} catch (IOException e) {
+			throw new IOException("Failed to write to file " + file.getPath()
+					+ " : " + e.getMessage());
+		} finally {
+			if (f != null) {
+				f.close();
+			}
+		}
 	}
 
 	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
