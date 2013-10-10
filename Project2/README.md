0f3d497af3b9d1e3f8cfa514ee360df30995fedd
Alex Cappiello <acappiel@andrew.cmu.edu>
2013-10-10 17:24:44 -0400

    Project 2 start.

---
+++ Project2/README.md
@@ -0,0 +1,13 @@
+Project 2: RMI
+============================
+
+Gradle
+------
+
+`gradle assemble`: compile
+
+`gradle test`: run unit tests
+
+`gradle build`: assemble and test
+
+`gradle eclipse`: generate esclipse project files
---
+++ Project2/src/main/java/registry/Registry.java
@@ -0,0 +1,12 @@
+package registry;
+
+public class Registry {
+
+    static private String helpHeader = "Project 1: Process Migration. 15-440, Fall 2013.";
+    static private String helpFooter = "Alex Cappiello (acappiel) and Michael Ryan (mer1).";
+
+    public static void main(String[] args) {
+
+    }
+
+}
