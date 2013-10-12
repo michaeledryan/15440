7725ae3730cfb2df8a44f1fbd0dfeb403fc620a5
Alex Cappiello <acappiel@andrew.cmu.edu>
2013-10-12 16:35:17 -0400

    Commenting and reformatting of server code.

--- Project2/src/main/java/server/RMIServer.java
+++ Project2/src/main/java/server/RMIServer.java
@@ -1,73 +1,72 @@
 package server;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-
+import org.apache.commons.cli.*;
 import registry.Registry;
 import registry.RegistryProxy;
 
+/**
+ * @author Michael Ryan and Alex Cappiello
+ */
 public class RMIServer {
-	static private String helpHeader = "Project 2: RMI. 15-440, Fall 2013.";
-	static private String helpFooter =
+
+    static private String helpHeader = "Project 2: RMI. 15-440, Fall 2013.";
+    static private String helpFooter =
             "Alex Cappiello (acappiel) and Michael Ryan (mer1).";
 
-	private int port;
+    private int port;
 
-	private int rport = 8000;
-	private String registryHost = "";
-	private Registry registry;
+    private int rport = 8000;
+    private String registryHost = "";
+    private Registry registry;
     private String trace;
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public Registry getRegistry() {
-		return registry;
-	}
+    public Registry getRegistry() {
+        return registry;
+    }
 
-	public void startServer(String[] args) {
-		Options opt = new Options();
-		opt.addOption("p", "port", true, "Port to listen on. Default: 1099.");
-		opt.addOption("rp", "registry port", true,
+    public void startServer(String[] args) {
+        
+        Options opt = new Options();
+        opt.addOption("p", "port", true, "Port to listen on. Default: 1099.");
+        opt.addOption("rp", "registry port", true,
                 "Port to look for the registry on. Default: 8000.");
-		opt.addOption("r", "registry host", true,
+        opt.addOption("r", "registry host", true,
                 "Host to look for the registry on. Default: localhost.");
         opt.addOption("t", "trace", true, "Trace to run internally.");
-		opt.addOption("h", "help", false, "Display help.");
+        opt.addOption("h", "help", false, "Display help.");
 
-		CommandLineParser parser = new GnuParser();
-		try {
-			CommandLine cmd = parser.parse(opt, args);
-			if (cmd.hasOption("h")) {
-				HelpFormatter help = new HelpFormatter();
-				help.printHelp("server", helpHeader, opt, helpFooter, true);
-				System.exit(1);
-			}
-			System.out.println("Starting server...");
-			String portString = cmd.getOptionValue("p", "1099");
-			String rportString = cmd.getOptionValue("rp", "8000");
+        CommandLineParser parser = new GnuParser();
+        try {
+            CommandLine cmd = parser.parse(opt, args);
+            if (cmd.hasOption("h")) {
+                HelpFormatter help = new HelpFormatter();
+                help.printHelp("server", helpHeader, opt, helpFooter, true);
+                System.exit(1);
+            }
+            System.out.println("Starting server...");
+            String portString = cmd.getOptionValue("p", "1099");
+            String rportString = cmd.getOptionValue("rp", "8000");
             trace = cmd.getOptionValue("t", "test1");
-			registryHost = cmd.getOptionValue("r", "localhost");
-			try {
-				port = Integer.parseInt(portString);
-				rport = Integer.parseInt(rportString);
-			} catch (NumberFormatException e) {
-				System.err.printf("Invalid port number: %s\n", portString);
-				System.exit(1);
-			}
-			Listener l = new Listener(port);
-			registry = new RegistryProxy(registryHost, rport);
-			new Thread(l).start();
+            registryHost = cmd.getOptionValue("r", "localhost");
+            try {
+                port = Integer.parseInt(portString);
+                rport = Integer.parseInt(rportString);
+            } catch (NumberFormatException e) {
+                System.err.printf("Invalid port number: %s\n", portString);
+                System.exit(1);
+            }
+            Listener l = new Listener(port);
+            registry = new RegistryProxy(registryHost, rport);
+            new Thread(l).start();
 
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     public String getTrace() {
         return trace;
