192048555362083596d23d1ede5f040905ba11c0
Alex Cappiello <acappiel@andrew.cmu.edu>
2013-10-12 16:35:33 -0400

    Break apart client.

--- Project2/src/main/java/client/Client.java
+++ Project2/src/main/java/client/Client.java
@@ -1,11 +1,7 @@
 package client;
 
 import org.apache.commons.cli.*;
-
-import registry.Registry;
 import registry.RegistryProxy;
-import remote.Remote440Exception;
-import tests.printer.RemotePrinter;
 
 /**
  * The client runs predefined test traces. The location of the registries are
@@ -15,19 +11,23 @@ import tests.printer.RemotePrinter;
  */
 public class Client {
 
-    static private String helpHeader = "Project 2: RMI. 15-440, Fall 2013.";
+    static private String helpHeader = "Project 2: RMI. 15-440, Fall 2013. " +
+            "-r and -p and be given multiple times.";
     static private String helpFooter =
             "Alex Cappiello (acappiel) and Michael Ryan (mer1).";
 
-    static private RegistryProxy[] registries;
+    private RegistryProxy[] registries;
+    private String trace;
 
-    public static void main(String[] args) {
+    public void startClient(String[] args) {
 
         Options opt = new Options();
         opt.addOption("r", "registry", true,
                 "Hostname of registry. Default: localhost.");
-        opt.addOption("p", "port", true, "Port to connect to. Default: 8000.");
-        opt.addOption("t", "trace", true, "Trace to run internally.");
+        opt.addOption("p", "port", true,
+                "Port to connect to registry. Default: 8000.");
+        opt.addOption("t", "trace", true,
+                "Trace to run internally. Default: test1.");
         opt.addOption("h", "help", false, "Display help.");
 
         CommandLineParser parser = new GnuParser();
@@ -39,7 +39,7 @@ public class Client {
                 System.exit(1);
             }
             System.out.println("Starting client...");
-            String trace = cmd.getOptionValue("t", "test1");
+            trace = cmd.getOptionValue("t", "test1");
 
             // CLI parsing gets a little ugly, since we have multiple
             // registries.
@@ -77,45 +77,18 @@ public class Client {
             System.out.printf("Connected to %d %s\n",
                     registries.length, word);
 
-            System.out.printf("Running trace: %s\n", trace);
-
-            // Pick the correct trace.
-            // Client and server should be on the same trace.
-            try {
-                switch (trace) {
-                    case "test1": {
-                        test1();
-                        break;
-                    }
-                    default: {
-                        System.err.printf("Unknown trace: %s\n", trace);
-                        System.exit(1);
-                    }
-                }
-            } catch (Remote440Exception e) {
-                e.printStackTrace();
-            }
-
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
     }
 
-    /**
-     * Really simple test on the ToyClass.
-     *
-     * @throws Remote440Exception
-     */
-    private static void test1() throws Remote440Exception {
-        Registry proxy = registries[0];
-        String item = "toy0";
-        System.out.printf("Looking up: %s\n", item);
-        RemotePrinter toy = (RemotePrinter) proxy.lookup(item);
-        if (toy != null) {
-            toy.printMessage("SHIT");
-            toy.printMessage("FOOBAR");
-        }
-
+    public RegistryProxy[] getRegistries() {
+        return registries;
     }
+
+    public String getTrace() {
+        return trace;
+    }
+
 }
--- Project2/src/main/java/tests/TestClient.java
+++ Project2/src/main/java/tests/TestClient.java
@@ -1,6 +1,61 @@
 package tests;
 
+import client.Client;
+import registry.Registry;
+import registry.RegistryProxy;
+import remote.Remote440Exception;
+import tests.printer.RemotePrinter;
+
 /**
  */
 public class TestClient {
+
+    private static Client client;
+    private static RegistryProxy[] registries;
+
+    public static void main(String[] args) {
+
+        client = new Client();
+        client.startClient(args);
+        registries = client.getRegistries();
+        String trace = client.getTrace();
+
+        System.out.printf("Running trace: %s\n", trace);
+
+        // Pick the correct trace.
+        // Client and server should be on the same trace.
+        try {
+            switch (trace) {
+                case "test1": {
+                    test1();
+                    break;
+                }
+                default: {
+                    System.err.printf("Unknown trace: %s\n", trace);
+                    System.exit(1);
+                }
+            }
+        } catch (Remote440Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Really simple test on the ToyClass.
+     *
+     * @throws Remote440Exception
+     */
+    private static void test1() throws Remote440Exception {
+        Registry proxy = registries[0];
+        String item = "toy0";
+        System.out.printf("Looking up: %s\n", item);
+        RemotePrinter toy = (RemotePrinter) proxy.lookup(item);
+        if (toy != null) {
+            toy.printMessage("SHIT");
+            toy.printMessage("FOOBAR");
+        }
+
+    }
+
 }
