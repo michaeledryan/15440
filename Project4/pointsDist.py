4fbafaada1b44b6595a14a1b1716f92a96676be5
Michael Ryan <michaeledryan@gmail.com>
2013-12-04 21:56:10 -0500

    numpy arrays

--- Project4/pointsDist.py
+++ Project4/pointsDist.py
@@ -69,38 +69,105 @@ datapoints = np.array(datapoints)
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 if rank == 0:
-  print datapoints
   centroids = {(x[0], x[1]): []  for x in random.sample(datapoints, numClusters)}
-  data = centroids.keys()
+  myKeys = centroids.keys()
+  for point in datapoints:  
+    dists = np.array([distance(point, x) for x in myKeys])
+    centroids[myKeys[np.argmin(dists)]].append((point[0], point[1]))
+  data = [(x, centroids[x]) for x in centroids.keys()]
 else:
   data = None
 data = comm.bcast(data, root=0)
 
-centroids = {data[rank-1] : []}
-print "bcast finished and data on rank %d is: "%comm.rank, data[rank-1]
-'''
+myCentroid = data[rank][0]
+datapoints = data[rank][1]
+
+print "Initial comms done, get on rank %d with centroid:"%rank, myCentroid
+
+centroids = {x[0]: [] for x in data}
+
+centroidRanks = {data[i][0]: i for i in xrange(len(data))}
 for i in xrange(numIters):
 
-  # put each point in a cluster with the nearest mean
+  # Reshuffle the closeness.
   for point in datapoints:
-    myKeys = centroids.keys()
+    myKeys = centroidRanks.keys()
     dists = np.array([distance(point, x) for x in myKeys])
     centroids[myKeys[np.argmin(dists)]].append(point)
 
-  # Calculate new means from the clusters
-  newCentroids = {}
-  for center in centroids.keys():
-    points = centroids[center]
-    newX = np.mean([x[0] for x in points])
-    newY = np.mean([y[1] for y in points])
-    newCentroids[(newX, newY)] = []
+  datapoints = np.array(centroids[myCentroid])
 
+  sendLengths = [0] * len(centroidRanks.keys())
+
+  # Send updates
+
+  means = centroidRanks.keys()
+  for mean in means:
+    if (mean != myCentroid):
+      comm.isend(len(centroids[mean]), dest=centroidRanks[mean], tag=0)
+  
+  for mean in means:
+    if (mean != myCentroid):
+      myData = comm.recv(source=centroidRanks[mean], tag=0)
+      sendLengths[centroidRanks[mean]] = myData
+      print "rank", rank, "got length:", sendLengths[centroidRanks[mean]]
+
+  print "RECEIVED LENGTHS"
+
+  # Send updates
+  means = centroidRanks.keys()
+  for mean in means:
+    if (mean != myCentroid):
+      print "rank", rank, "sending array of length", len(centroids[mean])
+      print np.array(centroids[mean])
+      comm.Isend([np.array(centroids[mean]), MPI.FLOAT], dest=centroidRanks[mean], tag=1)
+  
+  for mean in means:
+    if (mean != myCentroid):
+      #myData = np.zeros(sendLengths[centroidRanks[mean]] * 2, 'f')
+      #myData = np.zeros(10000, 'float32, float32')
+      myData = np.ndarray(shape=(sendLengths[centroidRanks[mean]], 2), dtype='f')
+
+      print "myData", myData
+      print "rank", rank, "expecting length", sendLengths[centroidRanks[mean]]
+      comm.Recv([myData, MPI.FLOAT], source=centroidRanks[mean], tag=1)
+      
+      if (myData.size != 0):
+        np.concatenate([datapoints, myData])
+
+  # Calculate new mean from the clusters
+  newMean = {}
+  newX = np.mean([x[0] for x in datapoints])
+  newY = np.mean([y[1] for y in datapoints])
+  
   #save old means as well
-  oldCentroids = centroids
+  oldCentroid = myCentroid
+  myCentroid = (newX, newY)
+
+  # Send updates
+  means = centroidRanks.keys()
+  for mean in means:
+    if (mean != myCentroid):
+      comm.isend(myCentroid, dest=centroidRanks[mean], tag=3)
+
+  
+  newCentroids = {myCentroid:[]}
+  newCentroidRanks = {myCentroid:rank}
+  for mean in means:
+    if (mean != myCentroid):
+      myData = comm.recv(source=centroidRanks[mean], tag=3)
+      newCentroids[myData] = []
+      newCentroidRanks[myData] = centroidRanks[mean]
+
+  centroidRanks = newCentroidRanks
   centroids = newCentroids
 
+  print rank, myCentroid
+
 # results
-writer = csv.writer(open(outfile, "w"))
-for centroid in oldCentroids:
-  writer.writerow([centroid[0], centroid[1]])
+'''
+if rank == 0:
+  writer = csv.writer(open(outfile, "w"))
+  for centroid in oldCentroids:
+    writer.writerow([centroid[0], centroid[1]])
 '''
